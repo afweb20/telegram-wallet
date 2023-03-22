07fb26ffc4273c92d3d7d24365a18149cb2a44dc
afweb20 <af_web@inbox.ru>
2023-03-22 21:13:10 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/telegramWallet.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "telegram-wallet"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./telegram-wallet"),
+    },
+    compress: true,
+    hot: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss|less|css)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource"
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./favicon.ico",
+      filename: "./index.html",
+      template: "./src/index.html"
+    })
+  ]
+};
